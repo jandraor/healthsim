2080830bde5782211c1bed4d906261d409b2e21b
Jair Andrade <jair.albert.andrade@gmail.com>
2018-07-05 16:03:31 +0100

    Updated HTML

--- app/templates.ts
+++ app/templates.ts
@@ -60,3 +60,12 @@ export const welcome = Handlebars.compile(`
 
   </div>
 `);
+
+export const alert = Handlebars.compile(`
+  <div class="alert alert-{{type}} alert-dismissible fade in" role="alert">
+    <button class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    {{message}}
+  </div>
+`);
