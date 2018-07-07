f1400e0851bcae3a5f065a49d324b44f13beb4a4
Jair Andrade <jair.albert.andrade@gmail.com>
2018-07-07 11:45:09 +0100

    Install and test D3 in front-end

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-social/bootstrap-social.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
+import * as d3 from 'd3';
+
 import 'bootstrap';
 
 import * as templates from './templates.ts';
@@ -40,6 +42,26 @@ const showView = async() => {
     case '#welcome':
       const session = await fetchJSON('/api/session');
       mainElement.innerHTML = templates.welcome({session});
+      //Width and height
+      var w = 500;
+      var h = 50;
+      const dataset = [ 5, 10, 15, 20, 25 ];
+
+      var svg = d3.select(".hs-main").append("svg");
+      console.log(dataset);
+      var circles = svg.selectAll("circle")
+                      .data(dataset)
+                      .enter()
+                      .append("circle");
+
+                      circles.attr("cx", function(d, i) {
+                return (i * 50) + 25;
+                })
+                .attr("cy", h/2)
+                .attr("r", function(d) {
+                return d;
+                });
+
       if (session.error) {
         showAlert(session.error);
       }
