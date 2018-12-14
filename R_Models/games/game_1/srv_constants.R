73dc29c0353290860007f8f764b095fe412274cf
jimduggan <jim.duggan@nuigalway.ie>
2018-12-14 17:51:06 +0000

    Changing root dir to healthsim

--- R_Models/games/game_1/srv_constants.R
+++ R_Models/games/game_1/srv_constants.R
@@ -1,3 +1,3 @@
-ROOT_DIR    <- "/Users/jim/Dropbox/HealthSim/R_Models/games/game_1/"
-CURRENT_DIR <- paste0(ROOT_DIR,"sim_data/current/")
-ARCHIVE_DIR <- paste0(ROOT_DIR,"sim_data/archive/")
+ROOT_DIR    <- "/Users/jim/Dropbox/HealthSim/"
+CURRENT_DIR <- paste0(ROOT_DIR,"R_Models/games/game_1/sim_data/current/")
+ARCHIVE_DIR <- paste0(ROOT_DIR,"R_Models/games/game_1/sim_data/archive/")
