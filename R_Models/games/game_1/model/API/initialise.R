d54b973497d3c698e99dd3bf5ce028714bcda3ac
Jim Duggan <jim.duggan@nuigalway.ie>
2018-12-18 18:58:41 +0000

    UPdating initialise

--- R_Models/games/game_1/model/API/initialise.R
+++ R_Models/games/game_1/model/API/initialise.R
@@ -14,7 +14,7 @@ initialise <- function(g_auxs,
                          ALPHA       = ALPHA,
                          seed        = seed)
   
-  sd_model   <- build_model(model_data$g_countries)
+  sd_model   <- build_model(model_data$g_countries, SAMPLE)
   
   model_data$g_stocks       <- sd_model$g_stocks
   model_data$g_NUM_SECTORS  <- sd_model$g_NUM_SECTORS
