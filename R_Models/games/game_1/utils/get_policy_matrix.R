d8e6ebb88f7b804411e54c216b1e748268ac2a7b
Jair Andrade <jair.albert.andrade@gmail.com>
2018-12-21 17:19:01 +0000

    Fixed get policy matrix function

--- R_Models/games/game_1/utils/get_policy_matrix.R
+++ R_Models/games/game_1/utils/get_policy_matrix.R
@@ -1,5 +1,8 @@
+suppressMessages(library(readr))
 get_policy_matrix <- function(countries){
-  pm <- as.matrix(read_csv("./R_Models/games/game_1/model/data/PolicyMatrix.csv"))
+  n_countries <- length(countries)
+  policyData <- read_csv("./R_Models/games/game_1/model/data/PolicyMatrix.csv")
+  pm <- as.matrix(policyData %>% slice(1:n_countries))
   rownames(pm) <- countries
   pm
 }
