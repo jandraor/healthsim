a203c401a0f1ab41e0817deca3def23b6ebde5ba
jimduggan <jim.duggan@nuigalway.ie>
2018-12-27 17:52:24 +0000

    Changing back to PolicyMatrix.csv

--- R_Models/games/game_1/utils/get_policy_matrix.R
+++ R_Models/games/game_1/utils/get_policy_matrix.R
@@ -1,7 +1,7 @@
 suppressMessages(library(readr))
 get_policy_matrix <- function(countries){
   n_countries <- length(countries)
-  policyData <- read_csv("./R_Models/games/game_1/model/data/PolicyMatrixTemplate.csv")
+  policyData <- read_csv("./R_Models/games/game_1/model/data/PolicyMatrix.csv")
   pm <- as.matrix(policyData %>% slice(1:n_countries))
   rownames(pm) <- countries
   pm
