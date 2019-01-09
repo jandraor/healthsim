684fd665b5e510c6f9704ed1b59ac2202ec548d2
Jair Andrade <jair.albert.andrade@gmail.com>
2019-01-09 12:07:56 +0000

    Fixed path

--- app/templates/player/main.ts
+++ app/templates/player/main.ts
@@ -1,6 +1,6 @@
 const $ = require('jquery');
 import * as wtng from './waiting.ts'
-import * as gameInterface from './game interface/main.ts'
+import * as gameInterface from './game_interface/main.ts'
 import * as mssg from './message.ts';
 
 
