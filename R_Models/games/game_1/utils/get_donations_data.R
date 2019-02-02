2088646c1cdc92c7ddac7100d0fd7f4e10b25be3
jimduggan <jim.duggan@nuigalway.ie>
2019-02-02 15:35:53 +0000

    Updating donations function

--- R_Models/games/game_1/utils/get_donations_data.R
+++ R_Models/games/game_1/utils/get_donations_data.R
@@ -1,5 +1,7 @@
 suppressMessages(library(readr))
-get_donations_data <- function(countries){
+suppressMessages(library(dplyr))
+get_donations_data <- function(start, stop){
   dt <- read_csv("./R_Models/games/game_1/model/data/donationsTemplate.csv")
+  dt <- mutate(dt,startTime=start,stopTime=stop)
   dt
 }
