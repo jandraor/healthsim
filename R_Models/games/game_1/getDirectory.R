79fc76801c47311ce32a09d8574f63adee79b3a3
Jair Andrade <jair.albert.andrade@gmail.com>
2019-05-17 16:04:56 +0100

    Fixing bug in directories

--- R_Models/games/game_1/getDirectory.R
+++ R_Models/games/game_1/getDirectory.R
@@ -1,4 +1,16 @@
 get_Directory <- function(typeDir) {
+  sim_data_path <- "./R_Models/games/game_1/sim_data/"
+  current_path <- paste0(sim_data_path, "current/")
+  archive_path <- paste0(sim_data_path, "archive/")
+  
+  if(!dir.exists(current_path)){
+    dir.create(current_path, recursive = T)
+  }
+  
+  if(!dir.exists(archive_path)){
+    dir.create(archive_path, recursive = T)
+  }
+  
   if (typeDir == 'current'){
     directory <- "./R_Models/games/game_1/sim_data/current/"
   }
