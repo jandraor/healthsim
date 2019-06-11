af7f23d9cb313fcf74648be2bdde47c0950b5af3
Jair Andrade <jair.albert.andrade@gmail.com>
2019-06-11 15:34:09 +0100

    Removing closing borders checkbox

--- app/templates/player/game_interface/inputDecisions/inputsR0.ts
+++ app/templates/player/game_interface/inputDecisions/inputsR0.ts
@@ -4,8 +4,6 @@ export const html = Handlebars.compile(`
   <div class = "d-block mt-2">
     <input type = "checkbox" id = "iptQuarantine">
       <span class = "text-success"> Quarantine </span>
-    <input type = "checkbox" name = "closeBorders" value = "closeBorders" class = "ml-5">
-    <span class = "text-success"> Close borders </span>
   </div>
 
 `);
