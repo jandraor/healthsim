233170a7a654f75aa54e88b853050a40ce3e4f28
Jair Andrade <jair.albert.andrade@gmail.com>
2019-08-19 18:43:05 +0100

    Fixing donation parser

--- R_Models/games/game_1/utils/convert_donatons_to_list.R
+++ R_Models/games/game_1/utils/convert_donatons_to_list.R
@@ -1,15 +1,18 @@
 suppressMessages(library(purrr))
 suppressMessages(library(dplyr))
 convert_donations_to_list <- function(donations){
-  countries <- factor(x = c('Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 
-                            'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa'),
-                      ordered = T);
+  countries_pool <- c('Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 
+                      'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa')
+  
+  nteams <- donations$from %>% unique() %>% length()
+  countries_used <- countries_pool[1:nteams]
+  countries <- factor(x = countries_used, ordered = T)
   
   aggregate_donations <- map(unique(donations$resource), function(res){
     df1         <- filter(donations,resource==res)
     agg_df1     <- df1 %>% group_by(from, to, resource) %>% 
       summarise(amount = sum(amount)) %>% ungroup()
-    nteams <- agg_df1 %>% pull(from) %>% unique %>% length()
+    
     spr_df1     <- spread(agg_df1, key = from, value = amount)
     mat_spr_df1 <- as.matrix(spr_df1 %>% select(-to, -resource))
     rownames(mat_spr_df1) <- colnames(mat_spr_df1)
